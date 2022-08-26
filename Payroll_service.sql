cab458510f740d7f316dfc4bb35ad246ae59a9c0
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-26 20:56:13 +0530

    records retrieved using select command

--- Payroll_service.sql
+++ Payroll_service.sql
@@ -22,3 +22,7 @@ insert into employee_payroll values
 ('Bill',10000.0,'2019-01-05'),
 ('Terisa',23000.0,'2017-08-12'),
 ('Charlie',15000.0,'2018-06-25')
+
+-------UC-4----
+
+select * from employee_payroll
