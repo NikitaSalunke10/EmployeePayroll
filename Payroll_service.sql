0f896af3292f4e2df18a43e1c80036c85364db1f
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-26 20:47:30 +0530

    employee_payroll table created

--- Payroll_service.sql
+++ Payroll_service.sql
@@ -5,3 +5,13 @@ create database payroll_service
 select name from sys.databases
 
 use payroll_service
+
+------UC-2------
+
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(100) not null,
+salary money not null,
+start_date date not null
+)
