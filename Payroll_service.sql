ecac492a8fab36f64a185c80fc207e3b1b25a5b7
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-26 20:51:24 +0530

    Inserted records in employee-payroll table

--- Payroll_service.sql
+++ Payroll_service.sql
@@ -15,3 +15,10 @@ name varchar(100) not null,
 salary money not null,
 start_date date not null
 )
+
+------UC-3------
+
+insert into employee_payroll values
+('Bill',10000.0,'2019-01-05'),
+('Terisa',23000.0,'2017-08-12'),
+('Charlie',15000.0,'2018-06-25')
